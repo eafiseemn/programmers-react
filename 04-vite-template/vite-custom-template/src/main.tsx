7bc4b9a7ffb7764d7f429466f66dd27ad3047034
eafiseemn <eafiseemn@gmail.com>
2025-07-07 15:48:10 +0900

    [React Core] Vite Manual Scaffolding Test

--- 04-vite-template/vite-custom-template/src/main.tsx
+++ 04-vite-template/vite-custom-template/src/main.tsx
@@ -1,13 +1,18 @@
+/* eslint-disable react/react-in-jsx-scope */
+
 /* -------------------------------------------------------------------------- */
 /*                           Vite Manual Scaffolding                          */
 /* -------------------------------------------------------------------------- */
 
-/* npm installations
-  "vite" : npm i vite -D
-  "react" / "react-dom" : npm i react react-dom
-  "TypeScript react/react-dom/node Type modules" : npm i @types/react @types/react-dom @types/node
-  "jsx compiler" : npm i @vitejs/plugin-react
-  "eslint" : npm init @eslint/config@latest
-    - options: javascript / problems / module / react / TypeScript yes / browser,node / install now / npm
-  "eslint hook/refresh plugins" : npm i -D eslint-plugin-react-hooks eslint-plugin-react-refresh
-*/
+import { createRoot } from 'react-dom/client'
+import Main from './components/Main';
+
+
+const container = document.getElementById('app');
+
+if (container) {
+  // Render
+  createRoot(container).render(<Main></Main>)
+} else {
+  console.warn("문서에 #app 요소가 존재하지 않습니다.")
+}
