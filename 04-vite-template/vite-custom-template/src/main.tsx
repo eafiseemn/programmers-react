8aaf4b9d299ec9f3c3efcf82f94066d8a4317237
eafiseemn <eafiseemn@gmail.com>
2025-07-08 09:31:49 +0900

    [React Core] Alias 사용하도록 수정

--- 04-vite-template/vite-custom-template/src/main.tsx
+++ 04-vite-template/vite-custom-template/src/main.tsx
@@ -5,7 +5,7 @@
 /* -------------------------------------------------------------------------- */
 
 import { createRoot } from 'react-dom/client'
-import Main from './components/Main';
+import Main from '@/components/Main';
 
 
 const container = document.getElementById('app');
