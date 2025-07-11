d85c4592a05103bd5efa8eeab5f3793aa20cf5dd
eafiseemn <eafiseemn@gmail.com>
2025-07-11 12:32:40 +0900

    [React Practice] Note App Page Router 설정

--- 09-react-component-states/src/pages/NoteApp/App.tsx
+++ 09-react-component-states/src/pages/NoteApp/App.tsx
@@ -1,18 +1,44 @@
 import { useState } from "react"
 import NoteListPage from "./pages/NoteListPage"
 import { getNoteList } from "./api/getNote"
+import { ROUTES } from "./routes";
 
 function NoteApp() {
 
   const [ list ] = useState(() => getNoteList());
     // 그냥 getNoteList() 를 넣으면 컴포넌트가 렌더링 될때마다 호출함
     // 콜백으로 넣으면 useState가 매번 getNoteList()를 호출하는 게 아니라 컴포넌트 최초 마운트 시 딱 한 번만 호출됨 (리렌더 되더라도 다시 호출하지 않음)
+  
+  /* noteId별 router 설정 */
+  const [ routeInfo, setRouteInfo ] = useState<{
+    route: string;
+    noteId: number | null;
+  }>({
+    route: 'list',
+    noteId: null
+  });
 
-  return (
-    <div>
-      <NoteListPage list={list} />
-    </div>
-  )
+  const handleChageRoute = (nextRoute:string, pickNoteId:number = 0) => {
+    setRouteInfo({
+      ...routeInfo,
+      route: nextRoute,
+      noteId: pickNoteId
+      // noteId: pickNoteId ? pickNoteId : routeInfo.noteId
+    })
+  }
+  
+  switch (routeInfo.route) {
+    case ROUTES.list:
+      return <NoteListPage list={list} onChangeRoute={handleChageRoute} />
+    case ROUTES.detail:
+      return <div>Detail Page</div>
+    case ROUTES.create:
+      return <div>Create Page</div>
+    case ROUTES.edit:
+      return <div>Edit Page</div>
+    default:
+      return <div>404 Not Found</div>
+  }
 }
 
 export default NoteApp
--- 09-react-component-states/src/pages/NoteApp/components/NoteList.tsx
+++ 09-react-component-states/src/pages/NoteApp/components/NoteList.tsx
@@ -1,12 +1,20 @@
 import { convertSlug } from "@/utils/convertSlug"
 import type { Note } from "../api/getNote"
 import './NoteList.css'
+import { ROUTES } from "../routes";
 
 interface Props {
   list: Note[]
+  onChangeRoute: (nextRoute:string, pickNoteId?:number) => void;
 }
 
-function NoteList({list}:Props) {
+function NoteList({list, onChangeRoute}:Props) {
+
+  const handleClick = (pickNoteId:number) => (e:React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    onChangeRoute(ROUTES.detail, pickNoteId);
+  }
+  // handler에 매개변수를 전달하려면 클릭했을 때 함수가 실행되고 -> 그 때 매개변수가 전달되는 구조가 되어야 함 => Closure 사용!
 
   return (
     <div className="NoteList">
@@ -16,7 +24,7 @@ function NoteList({list}:Props) {
           const slug = `#${convertSlug(item.title)}`
           return (
             <li key={item.id}>
-              <a href={slug}>{item.title}</a>
+              <a href={slug} onClick={handleClick(item.id)} >{item.title}</a>
             </li>)})}
       </ul>
     </div>
